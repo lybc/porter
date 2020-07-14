530dce04fd29f55f979297a86fa83ff14f2a442c
lybc <yibocheng.li@gmail.com>
2020-07-14 15:50:46 +0800

    格式化代码

--- commands/music.go
+++ commands/music.go
@@ -1,115 +1,115 @@
 package commands
 
 import (
-    "fmt"
-    "github.com/urfave/cli"
-    "net/url"
-    "porter/api"
-    "porter/utils"
-    "regexp"
-    "strconv"
+	"fmt"
+	"github.com/urfave/cli"
+	"net/url"
+	"porter/api"
+	"porter/utils"
+	"regexp"
+	"strconv"
 )
 
 const (
-    MusicMediaUrl = "http://music.163.com/song/media/outer/url?id=%s.mp3"
+	MusicMediaUrl = "http://music.163.com/song/media/outer/url?id=%s.mp3"
 )
 
 var musicCmd = cli.Command{
-    Name:  "music",
-    Usage: "下载音乐",
-    Flags: []cli.Flag{
-        &cli.StringFlag{
-            Name:  "output",
-            Usage: "下载文件输出路径",
-            Value: "./",
-        },
-    },
-    Action: func(c *cli.Context) error {
-        url := c.Args().Get(0)
-        if matchSingle, err := regexp.MatchString("https://music.163.com/song/*", url); matchSingle && err == nil {
-            return downloadSingle(c)
-        } else if matchPlaylist, err := regexp.MatchString("https://music.163.com/playlist/*", url); matchPlaylist && err == nil {
-            return downloadPlayList(c)
-        } else if matchRadio, err := regexp.MatchString("https://music.163.com/radio/*", url); matchRadio && err == nil {
-            return downloadRadio(url, c)
-        }
-        return nil
-    },
+	Name:  "music",
+	Usage: "下载音乐",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "output",
+			Usage: "下载文件输出路径",
+			Value: "./",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		url := c.Args().Get(0)
+		if matchSingle, err := regexp.MatchString("https://music.163.com/song/*", url); matchSingle && err == nil {
+			return downloadSingle(c)
+		} else if matchPlaylist, err := regexp.MatchString("https://music.163.com/playlist/*", url); matchPlaylist && err == nil {
+			return downloadPlayList(c)
+		} else if matchRadio, err := regexp.MatchString("https://music.163.com/radio/*", url); matchRadio && err == nil {
+			return downloadRadio(url, c)
+		}
+		return nil
+	},
 }
 
 func init() {
-    RootCmd.Commands = append(RootCmd.Commands, musicCmd)
+	RootCmd.Commands = append(RootCmd.Commands, musicCmd)
 }
 
 func getIdByUrl(resourceUrl string) string {
-    if u, err := url.Parse(resourceUrl); err == nil {
-        return u.Query().Get("id")
-    }
-    return ""
+	if u, err := url.Parse(resourceUrl); err == nil {
+		return u.Query().Get("id")
+	}
+	return ""
 }
 
 func downloadRadio(resourceUrl string, c *cli.Context) error {
-    //id := getIdByUrl(resourceUrl)
-    //radio := api.GetRadio(id)
-    //fmt.Println(c.String("output"))
-    //downloader := utils.NewDownloader(c.String("output"))
-    //for _, p := range radio.Programs {
-    //    downloader.AppendResource(p.MainSong.GetFileName(), p.MainSong.GetStreamUrl())
-    //}
-    //downloader.Start()
-    return nil
+	//id := getIdByUrl(resourceUrl)
+	//radio := api.GetRadio(id)
+	//fmt.Println(c.String("output"))
+	//downloader := utils.NewDownloader(c.String("output"))
+	//for _, p := range radio.Programs {
+	//    downloader.AppendResource(p.MainSong.GetFileName(), p.MainSong.GetStreamUrl())
+	//}
+	//downloader.Start()
+	return nil
 }
 
 // 下载歌单的歌曲
 func downloadPlayList(ctx *cli.Context) error {
-    u, err := url.Parse(ctx.Args().Get(0))
-    if err != nil {
-        return err
-    }
-    // 根据ID获取歌单
-    playlistId := u.Query().Get("id")
-    api := api.Netease{}
-    playList := api.GetPlayListDetail(playlistId)
-    if playList.Code != 200 {
-        return fmt.Errorf("获取网易云歌单详情失败")
-    }
-    fmt.Println(playList.Playlist.TrackCount)
+	u, err := url.Parse(ctx.Args().Get(0))
+	if err != nil {
+		return err
+	}
+	// 根据ID获取歌单
+	playlistId := u.Query().Get("id")
+	api := api.Netease{}
+	playList := api.GetPlayListDetail(playlistId)
+	if playList.Code != 200 {
+		return fmt.Errorf("获取网易云歌单详情失败")
+	}
+	fmt.Println(playList.Playlist.TrackCount)
 
-    var trackIds []string
-    for _, track := range playList.Playlist.TrackIds {
-        trackIds = append(trackIds, strconv.Itoa(track.ID))
-    }
-    // 根据歌曲ID获取歌曲详情，如果无需打印歌曲信息可省略
-    songsDetail := api.GetSongDetail(trackIds, false)
-    if songsDetail.Code != 200 {
-        return fmt.Errorf("获取歌曲详情失败")
-    }
+	var trackIds []string
+	for _, track := range playList.Playlist.TrackIds {
+		trackIds = append(trackIds, strconv.Itoa(track.ID))
+	}
+	// 根据歌曲ID获取歌曲详情，如果无需打印歌曲信息可省略
+	songsDetail := api.GetSongDetail(trackIds, false)
+	if songsDetail.Code != 200 {
+		return fmt.Errorf("获取歌曲详情失败")
+	}
 
-    downloader := utils.NewDownloader(ctx.String("output"))
-    for _, s := range songsDetail.Songs {
-        downloader.AppendResource(s.Name+".mp3", fmt.Sprintf(MusicMediaUrl, strconv.Itoa(s.ID)))
-    }
-    downloader.Start()
-    return nil
+	downloader := utils.NewDownloader(ctx.String("output"))
+	for _, s := range songsDetail.Songs {
+		downloader.AppendResource(s.Name+".mp3", fmt.Sprintf(MusicMediaUrl, strconv.Itoa(s.ID)))
+	}
+	downloader.Start()
+	return nil
 }
 
 func downloadSingle(ctx *cli.Context) error {
-    id := getIdByUrl(ctx.Args().First())
-    // 根据歌曲ID获取歌曲详情，如果无需打印歌曲信息可省略
+	id := getIdByUrl(ctx.Args().First())
+	// 根据歌曲ID获取歌曲详情，如果无需打印歌曲信息可省略
 
-    api := api.Netease{}
-    ids := []string{id}
-    songsDetail := api.GetSongDetail(ids, false)
-    if songsDetail.Code != 200 {
-        return fmt.Errorf("获取歌曲详情失败")
-    }
-    downloader := utils.NewDownloader(ctx.String("output"))
-    fmt.Println(downloader)
-    for _, song := range songsDetail.Songs {
-        name := fmt.Sprintf("%s.mp3", song.Name)
-        mp3Url := fmt.Sprintf(MusicMediaUrl, strconv.Itoa(song.ID))
-        fmt.Println(mp3Url)
-        downloader.AppendResource(name, mp3Url)
-    }
-    return downloader.Start()
+	api := api.Netease{}
+	ids := []string{id}
+	songsDetail := api.GetSongDetail(ids, false)
+	if songsDetail.Code != 200 {
+		return fmt.Errorf("获取歌曲详情失败")
+	}
+	downloader := utils.NewDownloader(ctx.String("output"))
+	fmt.Println(downloader)
+	for _, song := range songsDetail.Songs {
+		name := fmt.Sprintf("%s.mp3", song.Name)
+		mp3Url := fmt.Sprintf(MusicMediaUrl, strconv.Itoa(song.ID))
+		fmt.Println(mp3Url)
+		downloader.AppendResource(name, mp3Url)
+	}
+	return downloader.Start()
 }
